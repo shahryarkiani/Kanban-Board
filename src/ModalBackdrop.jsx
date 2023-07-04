e01263ebfd98aabd27ccffffabc171b81426c7fc
Shahryar K <shahryarkiani04@gmail.com>
2023-07-04 12:32:40 -0400

    Add code to check if user is highlighting before closing modal due to outside click

--- src/ModalBackdrop.jsx
+++ src/ModalBackdrop.jsx
@@ -1,9 +1,31 @@
+import {useEffect} from 'react'
 
 
 export default function ModalBackdrop({close, children}) {
 
+
+    useEffect(() => {
+
+        function closeOnEscPress(e) {
+            if(e.key === 'Escape')
+                close()
+        }
+
+        document.addEventListener('keydown', closeOnEscPress)
+
+        return () => {
+            document.removeEventListener('keydown', closeOnEscPress)
+        }
+
+    }, [])
+
+    function closeIfNotHighlighting(event) {
+        if(window.getSelection().toString() === '')
+            close()
+    }
+
     return(
-        <div onClick={close} className='absolute top-0 left-0 h-full w-full bg-white/80 flex justify-center items-center'>
+        <div onClick={closeIfNotHighlighting} className='absolute top-0 left-0 h-full w-full bg-white/80 flex justify-center items-center'>
             {children}
         </div>
     )
