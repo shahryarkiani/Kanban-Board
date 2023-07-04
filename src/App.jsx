15605ee23eb41262fca08e01c90f46f9008e41f3
Shahryar K <shahryarkiani04@gmail.com>
2023-07-04 10:16:16 -0400

    Implement drag and drop functionality, and add Done column

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,14 @@ import {DragDropContext} from '@hello-pangea/dnd'
 
 function App() {
 
-    const [backlogCards, setBacklogCards] = useState([new ticket('Drag and Drop', 'Implement DnD Functionality', 'High', '3H', 'Backlog', crypto.randomUUID())]);
-    const [todoCards, setTodoCards] = useState([new ticket('Drag and Drop', 'Implement DnD Functionality', 'High', '3H', 'To-Do', crypto.randomUUID()), new ticket('Drag and Drop', 'Implement DnD Functionality', 'High', '3H', 'To-Do', crypto.randomUUID()), new ticket('Drag and Drop', 'Implement DnD Functionality', 'High', '3H', 'To-Do', crypto.randomUUID()), new ticket('Drag and Drop', 'Implement DnD Functionality', 'High', '3H', 'To-Do', crypto.randomUUID()), new ticket('Drag and Drop', 'Implement DnD Functionality', 'High', '3H', 'To-Do', crypto.randomUUID()),new ticket('Drag and Drop', 'Implement DnD Functionality', 'High', '3H', 'To-Do', crypto.randomUUID()), new ticket('Drag and Drop', 'Implement DnD Functionality', 'High', '3H', 'To-Do', crypto.randomUUID()),new ticket('Drag and Drop', 'Implement DnD Functionality', 'High', '3H', 'To-Do', crypto.randomUUID()), new ticket('Drag and Drop', 'Implement DnD Functionality', 'High', '3H', 'To-Do', crypto.randomUUID()),new ticket('Drag and Drop', 'Implement DnD Functionality', 'High', '3H', 'To-Do', crypto.randomUUID()), new ticket('Drag and Drop', 'Implement DnD Functionality', 'High', '3H', 'To-Do', crypto.randomUUID())]);
-    const [inProgressCards, setInProgressCards] = useState([]);
+    const [backlogCards, setBacklogCards] = useState([new ticket('Drag and Drop', 'Implement DnD Functionality', 'High', '3H', 'Backlog', crypto.randomUUID())])
+    const [todoCards, setTodoCards] = useState([new ticket('Test1', 'Implement DnD Functionality', 'High', '3H', 'To-Do', crypto.randomUUID()), new ticket('Drag and Drop', 'Implement DnD Functionality', 'High', '3H', 'To-Do', crypto.randomUUID()), new ticket('Test2', 'Implement DnD Functionality', 'High', '3H', 'To-Do', crypto.randomUUID()), new ticket('Test3', 'Implement DnD Functionality', 'High', '3H', 'To-Do', crypto.randomUUID()), new ticket('Drag and Drop', 'Implement DnD Functionality', 'High', '3H', 'To-Do', crypto.randomUUID()),new ticket('Drag and Drop', 'Implement DnD Functionality', 'High', '3H', 'To-Do', crypto.randomUUID()), new ticket('Drag and Drop', 'Implement DnD Functionality', 'High', '3H', 'To-Do', crypto.randomUUID()),new ticket('Drag and Drop', 'Implement DnD Functionality', 'High', '3H', 'To-Do', crypto.randomUUID()), new ticket('Drag and Drop', 'Implement DnD Functionality', 'High', '3H', 'To-Do', crypto.randomUUID()),new ticket('Drag and Drop', 'Implement DnD Functionality', 'High', '3H', 'To-Do', crypto.randomUUID()), new ticket('Drag and Drop', 'Implement DnD Functionality', 'High', '3H', 'To-Do', crypto.randomUUID())])
+    const [inProgressCards, setInProgressCards] = useState([])
+    const [doneCards, setDoneCards] = useState([])
 
     function addCard(card) {
         const newCards = backlogCards.slice()
-        newCards.push(card)
+        newCards.unshift(card)
         setBacklogCards(newCards)
     }
 
@@ -27,14 +28,16 @@ function App() {
   return (
     <>
         <Sidebar openCreateModal={() => setCreateModalStatus(true)} />
-        <div className='flex ml-12 justify-evenly text-white items-start'>
-            <DragDropContext onDragEnd={console.log('DRAG ENDED')}>
+        <div className='flex ml-16 h-screen text-white items-start overflow-x-auto'>
+            <DragDropContext onDragEnd={handleDropEnd}>
 
                 <Cardholder title={'Backlog'} icon={<BsInboxes style={{color:'white', fontSize:'28'}} />} tickets={backlogCards} />
 
                 <Cardholder title={'To-Do'} icon={<FaArrowsToDot style={{color:'white', fontSize:'28'}}/>} tickets={todoCards} />
 
                 <Cardholder title={'In Progress'} icon={<TbProgressBolt style={{color:'white', fontSize:'28'}}/>} tickets={inProgressCards} />
+
+                <Cardholder title={'Done'} icon={<AiFillCheckCircle style={{color:'white', fontSize:'28'}}/>} tickets={doneCards} />
 
 
             </DragDropContext>
@@ -49,6 +52,81 @@ function App() {
 
     </>
   )
+
+
+    function handleDropEnd(result) {
+        console.log(JSON.stringify(result))
+
+        const source = result.source.droppableId
+
+        const originIndex = result.source.index
+        let originCards = null
+        let setOriginCards = null
+
+        if(source === 'Backlog'){
+            originCards = backlogCards
+            setOriginCards = setBacklogCards
+        }
+        else if(source === 'To-Do'){
+            originCards = todoCards
+            setOriginCards = setTodoCards
+        }
+        else if(source === 'In Progress'){
+            originCards = inProgressCards
+            setOriginCards = setInProgressCards
+        }
+        else if(source === 'Done'){
+            originCards = doneCards
+            setOriginCards = setDoneCards
+        }
+
+
+        const dest = result.destination.droppableId
+
+        let destCards = null
+        let setDestCards = null
+        const destIndex = result.destination.index
+
+        if(dest === 'Backlog'){
+            destCards = backlogCards
+            setDestCards = setBacklogCards
+        }
+        else if(dest === 'To-Do'){
+            destCards = todoCards
+            setDestCards = setTodoCards
+        }
+        else if(dest === 'In Progress'){
+            destCards = inProgressCards
+            setDestCards = setInProgressCards
+        }
+        else if(dest === 'Done'){
+            destCards = doneCards
+            setDestCards = setDoneCards
+        }
+
+
+        let newOriginCards = originCards.slice()
+
+        let temp = newOriginCards.splice(originIndex, 1)[0]
+
+
+        if(originCards === destCards) {
+            newOriginCards.splice(destIndex, 0, temp)
+            setOriginCards(newOriginCards)
+            return
+        }
+
+        let newDestCards = destCards.slice()
+
+        newDestCards.splice(destIndex, 0, temp)
+
+        setOriginCards(newOriginCards)
+
+        setDestCards(newDestCards)
+
+
+    }
+
 }
 
 export default App
